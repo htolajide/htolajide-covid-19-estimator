b8387daa6039d246979f32f03c651b82002d46b2
Hammed Taofeek <htolajide@yahoo.com>
2020-04-10 19:15:12 +0100

    severe-cases-by-req-time-defined

--- src/estimator.js
+++ src/estimator.js
@@ -2,13 +2,13 @@ const getImpactCurrentlyInfected = (inputData) => inputData * 10;
 const getSevereImpactCurrentlyInfected = (inputData) => inputData * 50;
 const infectionFactor = (periodType, number) => {
   let factor = 0;
-  const periodTypeLower = periodType.toLower();
+  const periodTypeLower = periodType.toLowerCase();
   if (periodTypeLower === 'days') {
-    factor = 2 ** (number / 3);
+    factor = 2 ** Math.floor(number / 3);
   } else if (periodTypeLower === 'weeks') {
-    factor = 2 ** (number / 21);
+    factor = 2 ** Math.floor(number / 21);
   } else if (periodTypeLower === 'months') {
-    factor = 2 ** (number / 90);
+    factor = 2 ** Math.floor(number / 90);
   }
   return factor;
 };
@@ -19,15 +19,19 @@ const covid19ImpactEstimator = (data = {}) => {
   const severeImpactCurrentlyInfected = getSevereImpactCurrentlyInfected(reportedCases);
   const impactInfecByReqTime = infectionFactor(periodType, timeToElapse) * impactCurrentlyInfected;
   const sevInfecByReq = infectionFactor(periodType, timeToElapse) * severeImpactCurrentlyInfected;
+  const impactSevCasesByRequestedTime = impactInfecByReqTime * (15 /100);
+  const sevSevCasesByReqTime = sevInfecByReq * (15 / 100);
   return {
     data: { data },
     impact: {
       currentlyInfected: impactCurrentlyInfected,
-      infectionsByRequestedTime: impactInfecByReqTime
+      infectionsByRequestedTime: impactInfecByReqTime,
+      severeCasesByrequestedTime: impactSevCasesByRequestedTime
     },
     severeImpact: {
       currentlyInfected: severeImpactCurrentlyInfected,
-      infectionsByRequestedTime: sevInfecByReq
+      infectionsByRequestedTime: sevInfecByReq,
+      severeCasesByrequestedTime: sevSevCasesByReqTime
     }
   };
 };
