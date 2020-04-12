2371cc4a466d5976c8b313410237e1b860e2a5f4
Hammed Taofeek <htolajide@yahoo.com>
2020-04-13 00:33:50 +0100

    ui-setup

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const path = require('path')
 // create a write stream (in append mode)
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 // create a write stream (in append mode)
-const requestLogStream = fs.createWriteStream(path.join(__dirname, 'request.json'), { flags: 'a' })
+// const requestLogStream = fs.createWriteStream(path.join(__dirname, 'request.json'), { flags: 'a' })
 
 const app = express();
 app.use(morgan('dev'));
 // app.use(morgan(':method :url :status :total-time[digits]', { stream: accessLogStream }))
 app.use(morgan(':method :url :status :response-time[digits] ms', { stream: accessLogStream }))
-app.use(morgan(':method :url :status :response-time[digits] ms', { stream: requestLogStream }))
+// app.use(morgan(':method :url :status :response-time[digits] ms', { stream: requestLogStream }))
 
 app.get('/hello/:name', (req, res) => {
   //res.setHeader('content-type', 'text/xml')
