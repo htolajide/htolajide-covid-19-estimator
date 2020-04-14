8616ae60282ffa87b93d2d41affa167111a72c61
Hammed Taofeek <htolajide@yahoo.com>
2020-04-14 09:35:29 +0100

    backend-setup

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 // Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
-require("@babel/register")({
-    presets: ["@babel/preset-env"]
+require('@babel/register')({
+    presets: ['@babel/preset-env']
   });
   // this help to resolve the import problems
   // npm install @babel/core @babel/register @babel/preset-env --save-dev
