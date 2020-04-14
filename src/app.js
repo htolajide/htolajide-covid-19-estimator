18b86e67bba4d6f2260a6517cf4324f79166faee
Hammed Taofeek <htolajide@yahoo.com>
2020-04-14 22:52:46 +0100

    backend-setup

--- src/app.js
+++ src/app.js
@@ -9,14 +9,13 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import objXml from 'object-to-xml';
 import covid19ImpactEstimator from './estimator';
-
 const logger = require('simple-node-logger').createSimpleLogger();
 
 dotenv.config();
 // store the port number
 const port = parseInt(process.env.PORT, 10) || 4500;
 // create a write stream (in append mode)
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.txt'), { flags: 'a' });
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,13 +23,17 @@ app.use(morgan('dev'));
 app.use(morgan((tokens, req, res) => {
   const realtime = tokens['response-time'](req, res);
   let time = Math.floor(realtime).toString();
-  if (time !== '0' && time.length === 1)time = `0${time}`;
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    `${time}ms`
-  ].join('\t');
+  let method = tokens.method(req, res);
+  let url = tokens.url(req, res);
+  let status = tokens.status(req, res);
+  if (time.length === 1)time = `0${time}`;
+  if (method.length < 4) method = `${method} `
+  if (url.includes('xml')) url = `${url} `
+  if (url.length < 20) {
+    status = `    ${status}`;
+    return `    ${method}   ${url}    ${status}   ${time} ms`;
+  }
+  return `    ${method}   ${url}    ${status}   ${time} ms`;
 }, { stream: accessLogStream }));
 // app.use(morgan(':method :url :status :total-time[digits]', { stream: accessLogStream }))
 // app.use(morgan(':method :url :status :response-time[0]ms', { stream: accessLogStream }));
@@ -69,8 +72,8 @@ app.post('/api/v1/on-covid-19/xml', (req, res) => {
 
 app.get('/api/v1/on-covid-19/logs', (req, res) => {
   res.setHeader('Content-Type', 'text/plain', 'charset=utf-8');
-  fs.readFile(path.join(__dirname, 'access.txt'), (err, data) => {
-    if (err) res.send('Error reading file');
+  fs.readFile(path.join(__dirname, 'access.log'), 'utf-8', (err, data) => {
+    if (err) res.send(`Error reading file  ${err}`);
     else if (data === undefined) res.send('No data');
     else { res.status(200).send(data.toString()); }
   });
